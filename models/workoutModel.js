30cff5d19c0b8bbd1973df22fb6cf5701207d944
Vincent Diaz <Vincent-Diaz@users.noreply.github.com>
2020-11-12 20:38:07 -0600

    added vitual method for totalDuration added schema

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -1,42 +1,53 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema ({
-
-    day: {
-        type: Date,
-        default: Date.now
+const WorkoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: true
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: true
+                },
+                duration: {
+                    type: Number
+                },
+                weight: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
     },
-    exercises: [
-        {
-            type: {
-                type: String,
-                trim: true,
-                required: true
-            },
-            name: {
-                type: String,
-                trim: true,
-                required: true
-            },
-            duration: {
-                type: Number
-            },
-            weight: {
-                type: Number
-            },
-            sets: {
-                type: Number
-            },
-            reps: {
-                type: Number
-            },
-            distance: {
-                type: Number
-            },
+    {
+        toJSON: {
+            virtuals: true
         }
-    ]
-    
+    }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercises) => {
+        return total + exercises.duration;
+    }, 0);
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
